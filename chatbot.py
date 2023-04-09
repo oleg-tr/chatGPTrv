758d82bd54ecb15d4b4ac68719f721bb7108d62e
oleg-tr <85102556+oleg-tr@users.noreply.github.com>
2023-04-10 00:24:54 +0300

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,103 @@
+import telebot
+import sys
+import openai
+import time
+import signal
+import config
+import creds
+
+
+global messages
+messages = config.settings
+
+openai.api_key = creds.OPENAI_API_KEY
+bot_token = creds.TELEGRAM_BOT_TOKEN
+
+print('Дед проснулся')
+
+def signal_handler(signal, frame):
+    print("Пизда рулю. Стоп-машина")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def code_from_text_new(i_messages):
+
+    if len(i_messages) > 20:
+        i_messages = i_messages[-20:]
+    print(i_messages)
+    for i in range(5): # Try up to 5 times
+        try:
+            response = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo",#-32k",
+              messages=i_messages,
+              temperature=0.85,
+              max_tokens=1000,
+              top_p=1.0,
+              frequency_penalty=0,
+              presence_penalty=0
+            )
+            res = response['choices'][0]['message']['content']
+
+            break # If successful, exit the loop
+        except Exception as e:
+            print("Error: ", e)
+            res = "Бля. Какая-то хуйня отъебнула. Можем повторить?"
+    else:
+        # If all retries fail, handle the error here
+        res = "Бля. Какая-то хуйня отъебнула. Можем повторить??"
+    return res
+
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Скажи пароль, друг, и входи')
+    print("Login attempt: ",message.from_user.username)
+    bot.register_next_step_handler(message, password_enter)
+
+@bot.message_handler(content_types=['text'])
+def password_enter(message):
+    password = message.text
+    lower_password = password.lower()
+    # Here goes authentication code
+    if( lower_password == "мусора позор россии"):
+        # If credentials are correct
+        bot.send_message(message.chat.id, 'Ну здорова. Кто тебя сюда пустил?')
+        print("Success login: ",message.from_user.username)
+        bot.register_next_step_handler(message, handle_message)
+        # If credentials are wrong
+    else:
+        bot.send_message(message.chat.id, 'Wrong credentials!')
+        print("Wrong password: ",message.from_user.username)
+        bot.register_next_step_handler(message, password_enter)
+
+@bot.message_handler(content_types=['text'])
+def handle_message(message):
+    global messages
+
+    if message.content_type == 'text':
+        text = message.text
+        # Call my function code_from_text
+        if (message.text=='/clear'):
+            messages=[{"role": "system", "content": "clearing"}]
+            bot.send_message(message.chat.id, 'Conversation cleared')
+        else:
+            start = time.time()
+            messages.append({"role": "user", "content": message.text})
+            result = code_from_text_new(messages)
+            end = time.time()
+            bot.send_message(message.chat.id,'generated in {0} sec:'.format(str(end - start)))
+            messages.append({"role": "assistant", "content": result})
+            bot.send_message(message.chat.id, result)
+    else:
+        bot.reply_to(message, "не ломай меня, пес")
+
+    bot.register_next_step_handler(message, handle_message)
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        print("И снова враги народа пытаются нас замедлить! Ошибка:", e)
+        time.sleep(5) # Пусть этот капиталистический код отдыхает, чтобы снова служить нашим интересам!
